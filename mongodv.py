5e22240d1bab5beecf698cd10cffb191c11a3a40
Diego Elias <diegonoblecilla@gmail.com>
2023-09-29 23:17:11 -0500

    Conexion MongoDB

--- mongodv.py
+++ mongodv.py
@@ -8,11 +8,9 @@ load_dotenv()
 user = os.getenv('MONGO_USER')
 password = os.getenv('MONGO_PASSWORD')
 
-uri = "mongodb+srv://{user}:{password}>@cluster0.blsgxxs.mongodb.net/?retryWrites=true&w=majority"
-
+uri = f"mongodb+srv://{user}:{password}@cluster0.siwckpr.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
 client = MongoClient(uri)
-
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
